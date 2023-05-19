f4f51c4ba4548d25c3320d3b72f85cad71c55f44
mehran banbersta <mehranbanbersta@mehrans-MBP.home>
2023-05-19 13:57:14 +0200

    commited init Api

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,9 @@
+import { initHomePage } from "./pages/homePages.js";
+// import { initApi } from './api.js';
 
 const loadApp = () => {
-  
+  // initApi()
+  initHomePage();
 };
 
 window.addEventListener("load", loadApp);
